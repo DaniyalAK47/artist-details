9fefe74c1bcd393eb8cf00b3da7968be14744741
DaniyalAK47 <58696581+DaniyalAK47@users.noreply.github.com>
2021-05-30 02:03:17 +0500

    functionality complete

--- src/Redux/actions/dataActions.js
+++ src/Redux/actions/dataActions.js
@@ -1,30 +1,81 @@
 import axios from "axios";
-import { GET_DATA } from "./dataActionTypes";
+import { GET_DATA, API_LOADING, INIT } from "./dataActionTypes";
 
-export const URL = "https://rest.bandsintown.com";
+export const URL = "https://rest.bandsintown.com/artists";
 
-export const getData = (link, params) => (dispatch) => {
+export const isLoading = (status) => (dispatch) => {
+  dispatch({
+    type: API_LOADING,
+    payload: {
+      response: status,
+    },
+  });
+};
+
+export const init = (status) => (dispatch) => {
+  dispatch({
+    type: INIT,
+    payload: {
+      response: status,
+    },
+  });
+};
+
+export const initEvent = (status) => (dispatch) => {
+  dispatch({
+    type: INIT,
+    payload: {
+      response: status,
+    },
+  });
+};
+
+export const getData = (params, link, query) => (dispatch) => {
+  dispatch(isLoading(true));
   var request = {};
-  request = {
-    method: "GET",
-    url: `${URL}/${link}?app_id=123123&date=${params}`,
-  };
+  if (link) {
+    request = {
+      method: "GET",
+      url: `${URL}/${params}/${link}?app_id=123123&date=${query}`,
+    };
+  } else {
+    request = {
+      method: "GET",
+      url: `${URL}/${params}?app_id=123123`,
+    };
+  }
 
   return axios(request)
     .then((response) => {
       console.log(response.data, "GETTTTTTT DATAAAAAAA");
-      dispatch({
-        type: GET_DATA,
-        payload: {
-          name: "get" + link,
-          response: response.data,
-        },
-      });
+      if (link) {
+        dispatch({
+          type: GET_DATA,
+          payload: {
+            name: "getartists" + link,
+            response: response.data,
+          },
+        });
+        dispatch(initEvent(false));
+      } else {
+        dispatch({
+          type: GET_DATA,
+          payload: {
+            name: "getartists",
+            response: response.data,
+          },
+        });
+        dispatch(init(false));
+      }
+      dispatch(isLoading(false));
+
       return response;
     })
     .catch((err) => {
       if (err) {
         console.log(err);
       }
+      dispatch(isLoading(false));
+      dispatch(init(false));
     });
 };
